2b67591fe3487d6f85b3eff177d0ce8b02d65c2c
YouvaTnst <youvatnst@gmail.com>
2024-01-19 23:52:57 +0100

    Adding Scroll function

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,3 +36,43 @@ const scrollUp = () =>{
                         : scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
+
+// Scroll section active link
+const sections = document.querySelectorAll('section[id]')
+const scrollActive = () =>{
+        const scrollDown = window.scrollY
+        sections.forEach(current =>{
+            const sectionHeight = current.offsetHeight,
+                  sectionTop = current.offsetTop - 58,
+                  sectionId = current.getAttribute('id'),
+                  sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+            if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+                sectionClass.classList.add('active-link')
+            }else{
+                sectionClass.classList.remove('active-link')
+            }
+        })
+}
+window.addEventListener('scroll', scrollActive)
+
+// Scroll reveal
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '40px',
+    opacity: 1,
+    scale: 1.1,
+    duration: 2500,
+    delay: 300,
+    // reset: true,
+})
+
+sr.reveal(`.home__data, .about__img, .about__data, .visit__data`)
+sr.reveal(`.home__image, .footer__img-1, .footer__img-2`, {rotate: {z: -15}})
+sr.reveal(`.home__bread, .about__bread`, {rotate: {z: 15}})
+sr.reveal(`.home__footer`, {scale: 1, origin: 'bottom'})
+sr.reveal(`.new__card:nth-child(1) img`, {rotate: {z: -30}, distance: 0})
+sr.reveal(`.new__card:nth-child(2) img`, {rotate: {z: 15}, distance: 0, delay: 600})
+sr.reveal(`.new__card:nth-child(3) img`, {rotate: {z: -30}, distance: 0, delay: 900})
+sr.reveal(`.favorite__card img`, {interval: 100, rotate: {z: 15}, distance: 0})
+sr.reveal(`.footer__container`, {scale: 1})
